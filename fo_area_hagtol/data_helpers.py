b6bd506bf0abe2faa8c92ba1775ac97166434e6f
Tim Wentzlau <tim@wentzlau.dk>
2023-09-26 22:52:20 +0100

    Update helper with all municipalities

--- fo_area_hagtol/data_helpers.py
+++ fo_area_hagtol/data_helpers.py
@@ -16,70 +16,169 @@ def get_filter_data():
             "px_id": "Norðoya region",
             "filter" : "agg:region-en.agg",
             "value": "4100",
-            "municipalities": {}
-        }
-        ,
+            "municipalities": {
+                "Fugloyar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4101"
+                },
+                "Viðareiðis kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4103"
+                },
+                "Hvannasunds kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4104"
+                },
+                "Klaksvíkar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4105"
+                },
+                "Kunoyar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4106"
+                }
+            }
+        },
         "Eysturoyar øki" : {
             "px_id": "Norðoya region",
             "filter" : "agg:region-en.agg",
             "value": "4200",
-            "municipalities": {}
+            "municipalities": {
+                "Fuglafjarðar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4203"
+                },
+                "Eysturkommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4205"
+                },
+                "Nes kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4206"
+                },
+                "Runavíkar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4207"
+                },
+                "Sjóvar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4208"
+                }
+                ,
+                "Eiðis kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4210"
+                },
+                "Sunda kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4211"
+                }
+            }
         },
         "Norðstreymoyar øki" : {
             "px_id": "Norðoya region",
             "filter" : "agg:region-en.agg",
             "value": "4300",
-            "municipalities": {}
+            "municipalities": {
+                "Kvívíkar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4306"
+                },
+                "Vestmanna kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4307"
+                }
+            }
         },
         "Suðurstreymoyar øki" : {
             "px_id": "Norðoya region",
             "filter" : "agg:region-en.agg",
             "value": "4700",
-            "municipalities": {}
+            "municipalities": {
+                "Tórshavnar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4701"
+                }
+            }
         },
         "Vága øki" : {
             "px_id": "Norðoya region",
             "filter" : "agg:region-en.agg",
             "value": "4400",
-            "municipalities": {}
+            "municipalities": {
+                "Vága kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4402"
+                },
+                "Sørvágs kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4403"
+                }
+            }
         },
         
         "Sandoy" : {
             "px_id": "Sandoyar region",
             "filter" : "agg:region-en.agg",
             "value": "4500",
-            "municipalities":{}
+            "municipalities":{
+               "Skopunar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4502"
+                },
+                "Sands kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4501"
+                },
+                "Skálavíkar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4503"
+                },
+                "Húsavíkar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4504"
+                },
+                "Skúgvoyar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4505"
+                } 
+            }
         },
         "Suðuroy" : {
             "px_id": "Suðuroyar region",
             "filter" : "agg:region-en.agg",
             "value": "4600",
             "municipalities": {
-                "tvo" : {
-                    "filter" : "agg:municipality-en-2017.agg",
-                    "value": "4602"
-                },
-                "famjin" : {
-                    "filter" : "agg:municipality-en-2017.agg",
-                    "value": "4603"
-                },
-                "hvalba" : {
+                "Hvalbiar kommuna" : {
                     "filter" : "agg:municipality-en-2017.agg",
                     "value": "4601"
                 },
-                "hov" : {
+                "Tvøroyrar kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4602"
+                },
+                "Fámjins kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4603"
+                },
+                
+                "Hovs kommuna" : {
                     "filter" : "agg:municipality-en-2017.agg",
                     "value": "4604"
                 },
-                "vag" : {
+                "Porkeris kommuna" : {
+                    "filter" : "agg:municipality-en-2017.agg",
+                    "value": "4605"
+                },
+                "Vágs kommuna" : {
                     "filter" : "agg:municipality-en-2017.agg",
                     "value": "4606"
                 },
-                "sumba" : {
+                "Sumbiar kommuna" : {
                     "filter" : "agg:municipality-en-2017.agg",
                     "value": "4607"
                 }
-            },
+            }
         }
         
     }
